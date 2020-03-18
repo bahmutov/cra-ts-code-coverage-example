9c277d59e9bbecd3c9a43ea942b2e6b30f28db39
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-18 11:23:06 -0400

    check code coverage after running the tests

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,7 +2,7 @@ export const add = (a:number, b:number) => a + b
 
 export const reverse = (s:string|null) => {
   if (s === null) {
-    return 'null'
+    return 'null' // this branch will not be called from the app
   } else {
     return s.split('').reverse().join('')
   }
